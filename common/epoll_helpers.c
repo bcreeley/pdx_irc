3af239802132bfd866d3cf5f6670af1085e6538e
bcreeley <bcreeley@pdx.edu>
2018-05-17 01:24:38 -0700

    Add new common epoll_helpers.c/.h for use by the client and the server

---
+++ common/epoll_helpers.c
@@ -0,0 +1,75 @@
+#include <sys/epoll.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include "epoll_helpers.h"
+
+int add_epoll_member(int epollfd, int memberfd, uint32_t subscribe_events)
+{
+	struct epoll_event epoll_ev;
+
+	epoll_ev.events = events;
+	epoll_ev.data.fd = memberfd;
+	if (epoll_ctl(epollfd, EPOLL_CTL_ADD, memberfd, &epoll_ev) == -1) {
+		perror("epoll_ctl: addfd");
+		return -1;
+	}
+
+	return 0;
+}
+
+int rm_epoll_member(int epollfd, int memberfd)
+{
+	if (epoll_ctl(epollfd, EPOLL_CTL_DEL, memberfd, NULL) == -1) {
+		perror("epoll_ctl: rmfd");
+		return -1;
+	}
+	close(memberfd);
+
+	return 0;
+}
+
+
+int create_epoll_manager(int *epollfd)
+{
+	if (!epollfd)
+		return -1;
+
+	*epollfd = epoll_create1(0);
+	if (*epollfd < 0) {
+		perror("epoll_create1");
+		return -1;
+	}
+
+	return 0;
+}
+
+int accept_new_member(int epollfd, int listenfd)
+{
+	int clientfd;
+
+	clientfd = accept(listenfd, (struct sockaddr *)NULL, NULL);
+	if (clientfd == -1) {
+		perror("accept");
+		return -1;
+	}
+
+	if (add_epoll_fd(epollfd, clientfd, EPOLL_NEW_CLIENT))
+		return -1;
+
+	return 0;
+}
+
+int debug_print_epoll_event(int eventfd, uint32_t event_mask)
+{
+	printf("fd=%d; events:%s%s%s%s%s%s%s\n",
+	       eventfd,
+	       (event_mask & EPOLLIN)      ? " EPOLLIN"      : "",
+	       (event_mask & EPOLLPRI)     ? " EPOLLPRI"     : "",
+	       (event_mask & EPOLLRDHUP)   ? " EPOLLRDHUP"   : "",
+	       (event_mask & EPOLLOUT)     ? " EPOLLOUT"     : "",
+	       (event_mask & EPOLLET)      ? " EPOLLET"      : "",
+	       (event_mask & EPOLLONESHOT) ? " EPOLLONESHOT" : "",
+	       (event_mask & EPOLLERR)     ? " EPOLLERR"     : "",
+	       (event_mask & EPOLLHUP)     ? " EPOLLHUP"     : "");
+}
+
---
+++ common/epoll_helpers.h
@@ -0,0 +1,18 @@
+#ifndef _EPOLL_HELPERS_H
+#define _EPOLL_HELPERS_H
+
+#define MAX_EPOLL_EVENTS 10
+
+#define SOCKET_EPOLL_DISCONNECT (EPOLLIN | EPOLLRDHUP)
+#define SOCKET_EPOLL_NEW_MEMBER (EPOLLIN | EPOLLRDHUP)
+
+#define for_each_epoll_event(i, nfds) \
+	for (i = 0; i < nfds; ++i)
+
+int create_epoll_manager(int *epollfd);
+int add_epoll_member(int epollfd, int memberfd, uint32_t subscribe_events);
+int rm_epoll_member(int epollfd, int memberfd);
+int accept_new_member(int epollfd, int listenfd);
+int debug_print_epoll_event(int eventfd, uint32_t event_mask);
+
+#endif /* _EPOLL_HELPERS_H */
