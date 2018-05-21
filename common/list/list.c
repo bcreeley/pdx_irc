6b098d341a8dd63f6d9657baa820ff9673c7aa18
bcreeley <bcreeley@pdx.edu>
2018-05-20 22:32:59 -0700

    Updated generic list implementation

--- common/list/list.c
+++ common/list/list.c
@@ -1,7 +1,16 @@
 #include "list.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+/**
+ * add_list_node - Adds a list_node right after the head of the list
+ *
+ * @head - head list_node of the list
+ * @add - list_node to be added
+ *
+ * Returns -1 on error, 0 on success
+ */
 int add_list_node(struct list_node *head, struct list_node *add)
 {
 	if (!head || !add)
@@ -14,44 +23,133 @@ int add_list_node(struct list_node *head, struct list_node *add)
 }
 
 //TODO: Note caller has to deallocate struct list_node *rm after calling this function
-int rm_list_node(struct list_node *prev, struct list_node *rm)
+
+/**
+ * rm_list_node - removes a list_node from the list and returns it
+ *
+ * @head - pointer to the head of the list so it can be updated if needed
+ * @data - data to find in a list_node of the list pointed to by *head
+ * @is_equal - function used for comparing data sent in with list_node data
+ *
+ * Returns the list_node with data matching the passed in data on success,
+ * otherwise returns NULL when a list_node with matching data cannot be found.
+ * Note: This function does not free the list_node but returns it after
+ * reconnecting the nodes around the found list_node.
+ */
+struct list_node *rm_list_node(struct list_node **head, void *data,
+			       bool (*is_equal)(void *d1, void *d2))
 {
-	if (!prev || !rm)
-		return -1;
+	struct list_node *tmp = *head;
 
-	prev->next = rm->next;
+	if (!tmp || !data)
+		return NULL;
 
-	return 0;
+	/* If the head of the list matches the data sent in */
+	if (is_equal(tmp->data, data)) {
+	       *head = (*head)->next;
+		return tmp;
+	}
+
+	while (tmp->next != NULL) {
+		struct list_node *prev = tmp;
+
+		tmp = tmp->next;
+		if (is_equal(tmp->data, data)) {
+			/* Found the node, remove and return it */
+			prev->next = tmp->next;
+			tmp->next = NULL;
+			return tmp;
+		}
+	}
+
+	/* Could not find data in the list */
+	return NULL;
+}
+
+/**
+ * list_contains - checks if the list has the data sent in as an argument
+ *
+ * @head: head of the list used to iterate through the list
+ * @data: data to look for in the list
+ * @is_equal: function used for comparing data sent in with list_node data
+ *
+ * Returns true if the data is found in the list, otherwise returns false
+ */
+bool list_contains(struct list_node *head, void *data,
+		   bool (*is_equal)(void *d1, void *d2))
+{
+	if (!data || !head)
+		return false;
+
+	for (head; head != NULL; head = head->next)
+		if (is_equal(head->data, data))
+			return true;
+
+	return false;
 }
 
 #if 0
 int main(int argc, char *argv[])
 {
 	struct list_node *head = NULL;
+
 	struct list_node *tmp = NULL;
+	struct channel c1, c2, c3;
 
-	head = (struct list_node *)calloc(1, sizeof(struct list_node));
+	strncpy(c1.name, "LinuxFTW!", sizeof("LINUXFTW!"));
+	strncpy(c2.name, "WindowsFTL!", sizeof("WindowsFTL"));
+	strncpy(c3.name, "GraduationFTW!", sizeof("GraduationFTW!"));
+
+	head = calloc(1, sizeof(struct list_node));
 	head->next = NULL;
-	head->data = 1;
+	head->data = (void *)&c1;
 
-
-	tmp = (struct list_node *)calloc(1, sizeof(struct list_node));
+	tmp = calloc(1, sizeof(struct list_node));
 	tmp->next = head->next;
 	head->next = tmp;
-	tmp->data = 2;
+	tmp->data = (void *)&c2;
 
 	for (tmp = head; tmp != NULL; tmp = tmp->next) {
-		printf("tmp->data = %d\n", tmp->data);
+		printf("tmp->data = %s\n", ((struct channel *)(tmp->data))->name);
 	}
 
-	tmp = head->next;
-	rm_list_node(head, tmp);
-	free(tmp);
+
 	for (tmp = head; tmp != NULL; tmp = tmp->next) {
-		printf("tmp->data = %d\n", tmp->data);
+		printf("tmp->data = %s\n", ((struct channel *)(tmp->data))->name);
 	}
 
-	free(head);
+	if (!list_contains(head, (void *)&c3, is_equal_channels))
+		printf("Could not find channel %s!\n", c3.name);
+
+	tmp = rm_list_node(&head, (void *)&c1, is_equal_channels);
+	if (tmp != NULL) {
+		printf("removed channel c1 %s\n", ((struct channel *)(tmp->data))->name);
+		free(tmp);
+	}
+
+	tmp = rm_list_node(&head, (void *)&c1, is_equal_channels);
+	if (tmp == NULL)
+		printf("Could not find channel c1 %s!\n", c1.name);
+
+	tmp = rm_list_node(&head, (void *)&c2, is_equal_channels);
+	if (tmp != NULL) {
+		printf("removed channel c2 %s\n", ((struct channel *)(tmp->data))->name);
+		free(tmp);
+	}
+
+	tmp = rm_list_node(&head, (void *)&c2, is_equal_channels);
+	if (tmp == NULL)
+		printf("Could not find channel c2 %s!\n", c2.name);
+
+#if 0 /* clear list */
+	while (head != NULL) {
+		tmp = head->next;
+
+		free(head);
+		head = tmp;
+	}
+#endif
 	return 0;
 }
 #endif
+
--- common/list/list.h
+++ common/list/list.h
@@ -1,12 +1,56 @@
 #ifndef _LIST_H
 #define _LIST_H
 
+#include "../protocol.h"
+#include <stdbool.h>
+#include <string.h>
+
+struct channel {
+	char name[CHANNEL_NAME_MAX_LEN];
+	char num_users;
+	struct user_list_node *user_list_head;
+};
+
+bool is_equal_channels(void *c1, void *c2)
+{
+	char *c1_name, *c2_name;
+
+	if (!c1 || !c2)
+		return false;
+
+	c1_name = ((struct channel *)c1)->name;
+	c2_name = ((struct channel *)c2)->name;
+	if (strcmp(c1_name, c2_name) == 0)
+		return true;
+
+	return false;
+}
+
+struct user {
+	char name[USER_NAME_MAX_LEN];
+	int fd;
+};
+
+bool is_equal_users(void *u1, void *u2)
+{
+	if (!u1 || !u2)
+		return false;
+
+	return (((struct user *)u1)->fd == ((struct user *)u2)->fd);
+}
+
 struct list_node {
 	struct list_node *next;
-	int data;
+	void *data;
 };
 
 int add_list_node(struct list_node *head, struct list_node *add);
-int rm_list_node(struct list_node *prev, struct list_node *rm);
+
+struct list_node *
+rm_list_node(struct list_node **head, void *data,
+	     bool (*is_equal)(void *d1, void *d2));
+
+bool list_contains(struct list_node *head, void *data,
+		   bool (*is_equal)(void *d1, void *d2));
 
 #endif /* _LIST_H */
